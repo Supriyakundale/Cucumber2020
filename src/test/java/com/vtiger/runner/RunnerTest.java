13e07b3d7f4850353e63fabbbbe0b79d07e4a351
Prashant Kurnawal <Prashant Kurnawal@Kurnawal>
2020-02-29 23:43:55 +0530

    first time commit

---
+++ src/test/java/com/vtiger/runner/RunnerTest.java
@@ -0,0 +1,27 @@
+package com.vtiger.runner;
+
+
+
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/com/vtiger/feature/Login.feature"
+		,glue= {"com.vtiger.stepdefinition"},
+		format = {"pretty", "html:target/Destination"} ,
+				//plugin = { "pretty", "json:target/cucumber.json" },
+				 //tags = {"@Smoke"},////or
+				// tags = {"@Smoke,@sanity"},////or
+				 //tags = {"@Smoke","@sanity"},////and
+				 //tags = {"@Smoke","~@Sanity"},////And
+				 //tags = {"~@Smoke","~@Sanity"},////And
+				 monochrome = true)
+
+
+
+public class RunnerTest {
+
+}
